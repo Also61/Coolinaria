bf9f4f1aa11c9a42aedbf3aa7ac1ef1d11c60145
Alexey Karpeev <alexey.karpeev@gmail.com>
2019-04-14 15:14:07 +0400

    v 1.0 OBSHEPITS

--- app/src/main/java/com/example/coolinaria/MainActivity.java
+++ app/src/main/java/com/example/coolinaria/MainActivity.java
@@ -20,18 +20,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ArrayList<RecipeItem> recipeItems = new ArrayList<>();
-//Добавление рецептов
-        recipeItems.add(new RecipeItem(R.drawable.tataki_roli,
-                Utils.TATAKI_TITLE, Utils.TATAKI_DESCRIPTION, Utils.TATAKI_RECIPE));
 
-        recipeItems.add(new RecipeItem(R.drawable.donburi,
-                Utils.DONBURI_TITLE,Utils.DONBURI_DESCRIPTION, Utils.DONBURI_RECIPE));
-
-        recipeItems.add(new RecipeItem(R.drawable.lasagna,
-                Utils.LASGANA_TITLE,Utils.LASGANA_DESCRIPTION,Utils.DONBURI_RECIPE));
-
-        recipeItems.add(new RecipeItem(R.drawable.glintwine,
-                Utils.GLINTVINE_TITLE,Utils.GLINTVINE_DESCRIPTION,Utils.GLINTVINE_RECIPE));
+        recipeItems.add(new RecipeItem(R.drawable.mak,Utils.McDONALDS_TITLE,Utils.McDONALDS_ADRESS,
+                Utils.McDONALDS_OTHER));
+        recipeItems.add(new RecipeItem(R.drawable.bk,Utils.BURGERKING_TITLE,Utils.BURGERKING_ADRESS,
+                Utils.BURGERKING_OTHER));
+        recipeItems.add(new RecipeItem(R.drawable.japonika,Utils.JAPONIKA_TITLE,Utils.JAPONIKA_ADRESS,
+                Utils.JAPONIKA_OTHER));
+        recipeItems.add(new RecipeItem(R.drawable.razdolie,Utils.RAZDOLIE_TITLE,Utils.RAZDOLIE_ADRESS,Utils.RAZDOLIE_OTHER));
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
